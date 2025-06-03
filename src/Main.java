95ab9cae1f645f1ce9e12cc5cf399114b7b13b12
mrjmgravity <sevenfold0198@gmail.com>
2025-06-03 20:56:18 +0200

    pridanie majitela domu pre prehladnost

--- src/Main.java
+++ src/Main.java
@@ -12,14 +12,14 @@ public class Main {
         ficovaVila.rozloha = 80f;
         ficovaVila.material = "tehla";
         ficovaVila.pocetOken = 250;
-        ficovaVila.vypocetCenyDomu(1);
+        ficovaVila.vypocetCenyDomu(1, "Robert Fico ");
 
         RodinnyDom kalinakovDom = new RodinnyDom();
         kalinakovDom.pocetPoschodi = 6;
         kalinakovDom.rozloha = 92f;
         kalinakovDom.material = "tehla";
         kalinakovDom.pocetOken = 322;
-        kalinakovDom.vypocetCenyDomu(2);
+        kalinakovDom.vypocetCenyDomu(2, "Robert Kalinak ");
 
 
     }
--- src/RodinnyDom.java
+++ src/RodinnyDom.java
@@ -5,8 +5,8 @@ public class RodinnyDom {
     float rozloha;
     int pocetOken;
 
-    public void vypocetCenyDomu(int cenaZaMeter){
-        System.out.println(rozloha * cenaZaMeter * pocetPoschodi);
+    public void vypocetCenyDomu(int cenaZaMeter, String menoDomu){
+        System.out.println(menoDomu + (rozloha * cenaZaMeter * pocetPoschodi));
     }
 
 }
