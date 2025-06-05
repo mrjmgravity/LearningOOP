769e58fccb1602b18bb4748d1f3f463992eef3b8
mrjmgravity <sevenfold0198@gmail.com>
2025-06-05 19:29:08 +0200

    Zakladne parametre triedy Obchod

--- src/Obchod.java
+++ src/Obchod.java
@@ -1,2 +1,24 @@
 public class Obchod {
+
+    private float rozloha;
+    private int pocetPoschodi;
+    private int pocetPokladni;
+
+
+    Obchod(float rozloha, int pocetPoschodi, int pocetPokladni) {
+
+        this.rozloha = rozloha;
+        this.pocetPoschodi = pocetPoschodi;
+        this.pocetPokladni = pocetPokladni;
+    }
+
+    public float vypocetCenyObchodu() {
+
+        float cena = (rozloha * pocetPoschodi) + 100 * pocetPokladni;
+        System.out.println("Cena obchodu" + cena);
+
+        return cena;
+
+    }
+
 }
