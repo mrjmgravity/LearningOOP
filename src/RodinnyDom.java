0449c30f0af201fa525d3889e7e5acdef4b4b0f0
mrjmgravity <sevenfold0198@gmail.com>
2025-06-05 18:55:52 +0200

    Vygenerovane getter a setter automaticky

--- src/RodinnyDom.java
+++ src/RodinnyDom.java
@@ -2,18 +2,47 @@ public class RodinnyDom {
 
     private int pocetPoschodi;
     private String material;
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public int getPocetPoschodi() {
+        return pocetPoschodi;
+    }
+
+    public void setPocetPoschodi(int pocetPoschodi) {
+        this.pocetPoschodi = pocetPoschodi;
+    }
+
+    public void setRozloha(float rozloha) {
+        this.rozloha = rozloha;
+    }
+
+    public int getPocetOken() {
+        return pocetOken;
+    }
+
+    public void setPocetOken(int pocetOken) {
+        this.pocetOken = pocetOken;
+    }
+
     private float rozloha;
     private int pocetOken;
 
-    public void zmenaRozlohy(float novaRozloha){
+    public void zmenaRozlohy(float novaRozloha) {
         rozloha = novaRozloha;
     }
 
-    public float getRozloha(){
+    public float getRozloha() {
         return rozloha;
     }
 
-    RodinnyDom(int pocetPoschodi, String material, float rozloha){
+    RodinnyDom(int pocetPoschodi, String material, float rozloha) {
         this.pocetPoschodi = pocetPoschodi;
         this.material = material;
         this.rozloha = rozloha;
@@ -26,7 +55,6 @@ public class RodinnyDom {
 
         return cena;
     }
-
 
 
 }
